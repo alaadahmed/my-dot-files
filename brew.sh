feb1193643ebb1f694d6864ab665cc5c5d2cbc0e
AlaaEldin Ahmed <alaadahmed@gmail.com>
2018-05-12 23:54:26 +0300

    update gem_list file

--- brew.sh
+++ brew.sh
@@ -46,26 +46,26 @@ fi;
 brew install wget --with-iri
 
 # Install more recent versions of some macOS tools.
-brew install macvim --with-override-system-vim --with-lua --with-python3
-brew install grep
+brew install macvim --with-override-system-vim --with-lua
+#brew install grep
 brew install wget
 
 # Install other useful binaries that I always use.
 brew install httpd
-brew install php71 --with-httpd
+brew install php72 --with-httpd
 brew install ctags 
 brew install fzf
-brew install git
-brew install git-flow
+#brew install git
+#brew install git-flow
 brew install heroku
-brew install lua
-brew install macvim --with-override-system-vim --with-lua --with-python3
+#brew install lua
+#brew install macvim --with-override-system-vim --with-lua
 brew install node
 brew install yarn
 brew install tree
-brew install imagemagick --with-webp
-brew install rbenv
-brew install sqlite
+#brew install imagemagick --with-webp
+#brew install rbenv
+#brew install sqlite
 brew install autoconf
 
 # Remove outdated versions from Cellar and cleanup.
